5319cf067b9b391b0ce4d4eaead410a2a1b9060b
Raiyyan khan <mdrayyankhan7273@gmail.com>
2026-04-02 00:30:05 +0530

    Add files via upload
    upload files

---
+++ app.js
@@ -0,0 +1,44 @@
+let input = document.querySelector("input");
+let button = document.querySelector("#task");
+let list = document.querySelector("#tasklist");
+
+
+let strtbtn = document.querySelector(".hide , button");
+
+let startpage = document.querySelector(".hide");
+let mainapp = document.querySelector(".container");
+
+
+
+button.addEventListener("click",function(){
+    let tasktext = input.value;
+
+    let li = document.createElement("li");
+      li.innerText =tasktext;
+
+    let dltbtn = document.createElement("button");
+    dltbtn.innerText="Delete";
+
+
+     li.appendChild(dltbtn);
+
+    list.appendChild(li);
+
+    input.value = "";
+})
+
+list.addEventListener("click", function (e) {
+    if (e.target.tagName === "BUTTON") {
+        e.target.parentElement.remove();
+    }
+});
+
+
+strtbtn.addEventListener("click", function(){
+    startpage.style.display = "none";
+    mainapp.style.display = "flex";
+     document.body.style.backgroundColor = "#CEB5A7";
+})   
+
+
+
